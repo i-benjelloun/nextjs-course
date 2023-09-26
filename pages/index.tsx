d3ad9d9657d53d094f150bc71cdca883ddbc12d6
i-benjelloun <ibenjelloun93@gmail.com>
2023-09-26 13:13:04 +0200

    navigate with "Link"

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h1>The home page</h1>
+      <li>
+        <Link href={"/portfolio"}>Portfolio</Link>
+      </li>
+      <li>
+        <Link href={"/clients"}>Clients</Link>
+      </li>
     </div>
   );
 }

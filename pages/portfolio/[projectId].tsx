d308101fa829f0e0bb6e39e2f02ddf6d51bdea8d
i-benjelloun <ibenjelloun93@gmail.com>
2023-09-26 12:52:06 +0200

    Adding dynamic Paths & Routes

---
+++ pages/portfolio/[projectId].tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PorfolioProjectPage() {
+  return (
+    <div>
+      <h1>The Portfolio Project Page</h1>
+    </div>
+  );
+}
+
+export default PorfolioProjectPage;

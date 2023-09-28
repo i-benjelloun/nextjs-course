f82105a1815a4a4b9102bd7170802f050632630a
i-benjelloun <ibenjelloun93@gmail.com>
2023-09-28 08:47:27 +0200

    some styles

--- components/events/EventItem.tsx
+++ components/events/EventItem.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+import styles from "./EventItem.module.css";
+
 function EventItem(props: Data) {
   const { title, image, date, location, id } = props;
   const readableDate = new Date(date).toLocaleDateString("en-US", {
@@ -13,19 +15,21 @@ function EventItem(props: Data) {
   const formattedAddress = location.replace(", ", "\n");
 
   return (
-    <li>
+    <li className={styles.item}>
       <Image src={"/" + image} alt={title} width={500} height={500} />
-      <div>
-        <h2>{title}</h2>
-        <div>
-          <time>{readableDate}</time>
+      <div className={styles.content}>
+        <div className={styles.summary}>
+          <h2>{title}</h2>
+          <div className={styles.date}>
+            <time>{readableDate}</time>
+          </div>
+          <div className={styles.address}>
+            <address>{formattedAddress}</address>
+          </div>
         </div>
-        <div>
-          <address>{formattedAddress}</address>
+        <div className={styles.actions}>
+          <Link href={`/events/${id}`}>Explore event</Link>
         </div>
-      </div>
-      <div>
-        <Link href={`/events/${id}`}>Explore event</Link>
       </div>
     </li>
   );
--- components/events/EventList.tsx
+++ components/events/EventList.tsx
@@ -2,9 +2,11 @@ import { Data } from "@/dummy-data";
 import React from "react";
 import EventItem from "./EventItem";
 
+import styles from "./EventList.module.css";
+
 function EventList({ events }: { events: Data[] }) {
   return (
-    <ul>
+    <ul className={styles.list}>
       {events.map((item: Data) => (
         <EventItem key={item.id} {...item} />
       ))}

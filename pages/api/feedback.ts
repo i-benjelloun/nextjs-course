c1f22e257aae28b35ec1431d66225320041bc407
i-benjelloun <ibenjelloun93@gmail.com>
2023-10-24 14:20:48 +0200

    POST request

--- pages/api/feedback.ts
+++ pages/api/feedback.ts
@@ -1,10 +1,37 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
+import fs from "fs";
 
-type ResponseData = { message: string };
+export interface Feedback {
+  id: string;
+  email: string;
+  text: string;
+}
+
+type ResponseData = { message: string; feedback?: Feedback };
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  res.status(200).json({ message: "this works" });
+  if (req.method === "POST") {
+    const email = req.body.email;
+    const feedbackText = req.body.text;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email,
+      text: feedbackText,
+    };
+
+    const filePath = path.join(process.cwd(), "data", "feedback.json");
+    const fileData = fs.readFileSync(filePath, "utf-8");
+    console.log(fileData);
+    const data = JSON.parse(fileData);
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({ message: "Success", feedback: newFeedback });
+  } else {
+    res.status(201).json({ message: "this works" });
+  }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,39 @@
 import Link from "next/link";
+import { FormEvent, useRef } from "react";
 
 export default function Home() {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const feedbackRef = useRef<HTMLTextAreaElement>(null);
+
+  function submitHandler(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const enteredEmail = emailRef.current?.value;
+    const enteredFeedback = feedbackRef.current?.value;
+
+    const requestBody = { email: enteredEmail, text: enteredFeedback };
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
   return (
     <div>
       <h1>The home page</h1>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="email">Your Email address</label>
+          <input type="email" id="email" ref={emailRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your feedback</label>
+          <textarea id="feedback" rows={5} ref={feedbackRef} />
+        </div>
+        <button type="submit">Send feedback</button>
+      </form>
     </div>
   );
 }
